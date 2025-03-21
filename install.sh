d32d10d6640fd8ef7e034b310dad89200806437e
sid_hsieh <sid_hsieh@phison.com>
2025-03-21 13:22:42 +0800

    GIP_1.3.0

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ fi
 
 #### Grafana Related
 GRAFANA_LISTEN_PORT_ON_HOST=3000
-VERSION_TAG=GIP_1.2.0
+VERSION_TAG=GIP_1.3.0
 GRAFANA_DOWNLOAD_LINK="https://github.com/phison-dev/grafana_install_package_v2/releases/download/$VERSION_TAG/grafana.tar"
 OTEL_DOWNLOAD_LINK="https://github.com/phison-dev/grafana_install_package_v2/releases/download/$VERSION_TAG/otel.tar"
 TEMPO_DOWNLOAD_LINK="https://github.com/phison-dev/grafana_install_package_v2/releases/download/$VERSION_TAG/tempo.tar"
